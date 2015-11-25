2ad254049dc64eaab21f10cdd0376629153cd520
GrandAdmiralGergar <gdorchies@gmail.com>
2015-11-24 23:38:06 -0500

    Added dashboard and pages

--- functions.js
+++ functions.js
@@ -3,6 +3,7 @@
 var sqlite3 = require('sqlite3').verbose(),
     Q = require('q'),
     crypto = require('crypto');
+    request = require('request')
 
 exports.hashPassword = function(password, salt) {
   var hash = crypto.createHash('sha256');
@@ -32,8 +33,8 @@ exports.localAuthorization = function (username, password) {
           else 
           {
              var user = {
-                "username" : username,
-                "id" : id
+                "username" : row.username,
+                "id" : row.id
              }
              console.log("PASSWORD MATCH");
              deferred.resolve(user);        
@@ -52,10 +53,8 @@ exports.localRegistration = function(username, password) {
    var hash = exports.hashPassword(password, salt);
    var user = {
       "username": username,
-      "id" : 0,
-      "password": hash,
-      "avatar": "http://placepuppy.it/images/homepage/Beagle_puppy_6_weeks.JPG"
-   }
+      "id" : 0
+   };
    db.get('SELECT username FROM users WHERE username=?', username, function(err, row) {
       if(!row)
       {
@@ -78,73 +77,21 @@ exports.localRegistration = function(username, password) {
    
 }
 
-////used in local-signup strategy
-//exports.localReg = function (username, password) {
-//  var deferred = Q.defer();
-//  var hash = bcrypt.hashSync(password, 8);
-//  var user = {
-//    "username": username,
-//    "password": hash,
-//    "avatar": "http://placepuppy.it/images/homepage/Beagle_puppy_6_weeks.JPG"
-//  }
-//  //check if username is already assigned in our database
-//  db.get('local-users', username)
-//  .then(function (result){ //case in which user already exists in db
-//    console.log('username already exists');
-//    deferred.resolve(false); //username already exists
-//  })
-//  .fail(function (result) {//case in which user does not already exist in db
-//      console.log(result.body);
-//      if (result.body.message == 'The requested items could not be found.'){
-//        console.log('Username is free for use');
-//        db.put('local-users', username, user)
-//        .then(function () {
-//          console.log("USER: " + user);
-//          deferred.resolve(user);
-//        })
-//        .fail(function (err) {
-//          console.log("PUT FAIL:" + err.body);
-//          deferred.reject(new Error(err.body));
-//        });
-//      } else {
-//        deferred.reject(new Error(result.body));
-//      }
-//  });
-//
-//  return deferred.promise;
-//};
-
-//check if user exists
-    //if user exists check if passwords match (use bcrypt.compareSync(password, hash); // true where 'hash' is password in DB)
-      //if password matches take into website
-  //if user doesn't exist or password doesn't match tell them it failed
-//exports.localAuth = function (username, password) {
-//  var deferred = Q.defer();
-//
-//  db.get('local-users', username)
-//  .then(function (result){
-//    console.log("FOUND USER");
-//    var hash = result.body.password;
-//    console.log(hash);
-//    console.log(bcrypt.compareSync(password, hash));
-//    if (bcrypt.compareSync(password, hash)) {
-//      deferred.resolve(result.body);
-//    } else {
-//      console.log("PASSWORDS NOT MATCH");
-//      deferred.resolve(false);
-//    }
-//  }).fail(function (err){
-//    if (err.body.message == 'The requested items could not be found.'){
-//          console.log("COULD NOT FIND USER IN DB FOR SIGNIN");
-//          deferred.resolve(false);
-//    } else {
-//      deferred.reject(new Error(err));
-//    }
-//  });
-//
-//  return deferred.promise;
-//}
-
-
-
-
+exports.getRandomWikiPage = function(callback) {
+   var result = "";
+   request({
+      uri: "http://en.wikipedia.org/wiki/Special:Random",
+      }, function(error, response, body) {
+      
+      var array = body.match(/<a href="\/wiki\/[^:><]*?" title="([\s\S])*?">[\s\S]*?<\/a>/gm);
+//      body = body.replace(/[\s\S]*?<div id="mw-content-text" lang="en" dir="ltr" class="mw-content-ltr">/gm, '<div id="mw-content-text" lang="en" dir="ltr" class="mw-content-ltr">');
+//      body = body.replace(/\/wiki\//ig, "http://en.wikipedia.org/wiki");
+//      body = body.replace(/\/\/upload.wikimedia.org/ig, "http://upload.wikimedia.org");
+//      body = body.replace(/<sup[\s\S]*?<\/sup>/gm, "");
+//      body = body.replace(/<div class="reflist[\s\S]*?<\/div>/gm, "");
+//      body = body.replace(/<h2><span class="mw-headline" id="References">[\s\S]*/gm, "</div>");
+      //body = body.replace(/<noscript>[\s\S]*/gm, "</div>");
+      result = array.join("<br>\n");      
+      callback(result);
+    });
+}
